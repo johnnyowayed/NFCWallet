bf2fb10b9246db2bf8ce5f793ab6c28d5444ac61
Johnny Owayed <johnny.owayed@gmail.com>
2025-05-06 11:43:49 +0300

    Initial Commit

---
+++ NFCWallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NFCWallet
+//
+//  Created by Johnny Owayed on 06/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NFCWallet/NFCWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCWalletApp.swift
+//  NFCWallet
+//
+//  Created by Johnny Owayed on 06/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct NFCWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
